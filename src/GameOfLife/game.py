0f3e92d330fed895b7cc46fb3179b0596ea07781
Eloi Dieme <eloidieme@gmail.com>
2024-04-03 21:49:13 +0200

    fix: fixed grid initialization and wrote tests for random and dead init

--- src/GameOfLife/game.py
+++ src/GameOfLife/game.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from GameOfLife import logger
 
+
 class Game:
     def __init__(
             self,
@@ -13,25 +14,25 @@ class Game:
             random_grid: bool = True,
             starting_grid_filepath: Optional[str] = None,
             random_seed: Optional[int] = None,
+            alive_probability: float = 0.5
     ) -> None:
         if grid_height <= 0 or grid_width <= 0:
             logger.error("Grid width and height must be positive.")
             raise ValueError
-        
+
+        self.grid_size = (grid_width, grid_height)
+        self.starting_grid_filepath = starting_grid_filepath
+        self.random_grid = random_grid
+        if starting_grid_filepath:
+            self.random_grid = False
         if random_seed:
             np.random.seed(random_seed)
-        
-        self.grid_size = (grid_width, grid_height)
-        if starting_grid_filepath:
-            self.starting_grid_filepath = starting_grid_filepath
-            self.random_grid = False
-        else:
-            self.random_grid = random_grid
+        self.alive_probability = alive_probability
 
-    def _parse_grid_from_file(self):
+    def _parse_grid_from_file(self, path: str):
         pass
 
-    def _initialize_grid(self) -> None:
+    def _initialize_grid(self) -> np.ndarray:
         """
         Initializes a grid using the class attributes (random or file-loaded).
         Defaults to a dead grid (meaning all the cells are set to zero).
@@ -42,7 +43,8 @@ class Game:
             grid = self._parse_grid_from_file()
 
         if self.random_grid:
-            grid = np.random.choice([False, True], self.grid_size)
+            grid = np.random.choice([0, 1], self.grid_size, p=[
+                                    1 - self.alive_probability, self.alive_probability])
 
         return grid
 
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,9 @@
 import pytest
+import numpy as np
 
 from GameOfLife.game import Game
+
+np.random.seed(42)
 
 def test_incorrect_grid_size():
     with pytest.raises(ValueError):
@@ -11,3 +14,11 @@ def test_incorrect_grid_size():
         game = Game(-1, 1)
     with pytest.raises(ValueError):
         game = Game(1, -1)
+
+def test_initialize_dead_grid():
+    game = Game(100, 100, random_grid=False)
+    assert np.all(game._initialize_grid() == 0)
+
+def test_initialize_random_grid():
+    game = Game(100, 100)
+    assert np.any(game._initialize_grid() == 1) 
